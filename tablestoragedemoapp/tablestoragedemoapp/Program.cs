c125ee99a89d6c2a037c804a652d44f68d5b96e6
Krishna Bhagavan <basvani.bhagavan@ikea.com>
2019-11-27 06:46:23 +0100

    added code

--- tablestoragedemoapp/tablestoragedemoapp/Program.cs
+++ tablestoragedemoapp/tablestoragedemoapp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace tablestoragedemoapp
 {
@@ -10,6 +12,13 @@ namespace tablestoragedemoapp
     {
         static void Main(string[] args)
         {
+            CloudStorageAccount sa = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=krishnasademo;AccountKey=tImhMEV5Q+nu+MKDo5P9ycUXzXnwUUlO63sjrmdrrOXsTcLYunOIyZu/Bmw8IMUeKumaqfXMpE/roFcMdAklnw==;EndpointSuffix=core.windows.net");
+            CloudTableClient tableClient = sa.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("employee");
+
+
         }
     }
+
+
 }
